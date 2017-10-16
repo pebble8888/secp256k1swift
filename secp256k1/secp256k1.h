3ffa5f4e455bd0284597db682dcd65de59831d76
pebble8888 <pebble8888@gmail.com>
2017-10-16 22:05:26 +0900

    Initial Commit

---
+++ secp256k1/secp256k1.h
@@ -0,0 +1,19 @@
+//
+//  secp256k1.h
+//  secp256k1
+//
+//  Created by pebble8888 on 2017/10/16.
+//  Copyright © 2017年 pebble8888. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for secp256k1.
+FOUNDATION_EXPORT double secp256k1VersionNumber;
+
+//! Project version string for secp256k1.
+FOUNDATION_EXPORT const unsigned char secp256k1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <secp256k1/PublicHeader.h>
+
+
